77b0e601e2e15e47c4866a805629fcd5e3f11caf
SarahH <sarah.heanan@komododigital.co.uk>
2023-08-21 17:48:39 +0100

    create child component - ExpenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,11 @@
+import ExpenseDate  from "./ExpenseDate";
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const date = props.expense.date;
-    const month = date.toLocaleString('en-GB', { month: 'long' });
-    const year = date.getFullYear();
-    const day = date.toLocaleString('en-GB', { day: '2-digit' });
-
+    console.log(props.expense.date);
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={props.expense.date}/>
             <div className="expense-item__description">
                 <h2>{props.expense.title}</h2>
                 <div className="expense-item__price">${props.expense.amount}</div>
