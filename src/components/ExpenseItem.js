35c571c9da6bd78b11639311438f21b4fea734f7
SarahH <sarah.heanan@komododigital.co.uk>
2023-08-21 17:31:06 +0100

    transform date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,18 @@
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    const date = props.expense.date;
+    const month = date.toLocaleString('en-GB', { month: 'long' });
+    const year = date.getFullYear();
+    const day = date.toLocaleString('en-GB', { day: '2-digit' });
 
     return (
         <div className="expense-item">
-            <div>{props.expense.date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{year}</div>
+                <div>{day}</div>
+            </div>
             <div className="expense-item__description">
                 <h2>{props.expense.title}</h2>
                 <div className="expense-item__price">${props.expense.amount}</div>
