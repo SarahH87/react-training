b0a2e10b87fd5509b121fe964c63f5b7963fb993
SarahH <sarah.heanan@komododigital.co.uk>
2023-08-25 14:10:01 +0100

    basic example of using state for title

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,27 @@
+import React, { useState } from 'react';
+
 import Card from "../UI/Card";
 import ExpenseDate  from "./ExpenseDate";
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    console.log(props.expense.date);
+    //value + function to set value
+    //useState always returns these things
+    const [title, setTitle] = useState(props.expense.title);
+
+    const clickHandler = () => {
+        setTitle('updated');
+        console.log(title)
+    };
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.expense.date}/>
             <div className="expense-item__description">
-                <h2>{props.expense.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">${props.expense.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
